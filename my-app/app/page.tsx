1ca5e2aebd0fc155da7eb436fc0cd80ac296cfba
Simone Poggiali <gibbok.coding@gmail.com>
2024-12-30 11:45:45 +0100

    add content

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="uppercase text-2xl">Server Components</h1>
-      <h2 className="mb-5">Static Rendering</h2>
+      <h1 className="uppercase text-4xl">Server Components</h1>
+      <h2 className="text-3xl mb-5">Static Rendering</h2>
       <p className="mb-5">
         Server Components allow you to write UI that can be rendered and
         optionally cached on the server.
@@ -15,6 +15,13 @@ export default function Home() {
         <li>Dynamic Rendering</li>
         <li>Streaming</li>
       </ul>
+      <h3 className="text-2xl my-8">Static Rendering</h3>
+      <p>
+        This route is rendered at build time, or in the background after data
+        revalidation. The result is cached and can be pushed to a Content
+        Delivery Network (CDN). This optimization allows you to share the result
+        of the rendering work between users and server requests.
+      </p>
     </div>
   )
 }
