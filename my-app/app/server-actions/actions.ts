7358d5395a5730447c783a05f7feccac9830f66f
Simone Poggiali <gibbok.coding@gmail.com>
2025-01-01 12:52:46 +0100

    s

--- my-app/app/server-actions/actions.ts
+++ my-app/app/server-actions/actions.ts
@@ -2,11 +2,13 @@
 
 import { revalidatePath } from 'next/cache'
 
+// This a server action, is a function which will be accessible via the client.
+// Next.js under the hook will make a POST request.
 export async function createComment(formData: FormData) {
   await fetch('http://localhost:3001/comment', {
     method: 'POST',
     body: formData,
   })
 
-  revalidatePath('server-actions')
+  revalidatePath('/server-actions')
 }
--- my-app/app/server-actions/page.tsx
+++ my-app/app/server-actions/page.tsx
@@ -13,6 +13,30 @@ export default function Page() {
         Behind the scenes, actions use the POST method, and only this HTTP
         method can invoke them.
       </p>
+      <p className="mb-5">
+        Server Actions are functions. This means they can be reused anywhere in
+        your application.
+      </p>
+      <ul className="list-disc ml-2 mb-5">
+        <li>
+          Server Components support progressive enhancement by default. Works
+          even with no JS enabled.
+        </li>
+        <li>
+          In Client Components, forms invoking Server Actions will queue
+          submissions if JavaScript isn&apos;t loaded yet, prioritizing client
+          hydration.After hydration, the browser does not refresh on form
+          submission.
+        </li>
+        <li>Server Actions can be invoked from event handlers or useEffect.</li>
+        <li>
+          Server Actions integrate caching and revalidation architecture. When
+          an action is invoked, Next.js can return both the updated UI and new
+          data in a single server roundtrip.
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
       <p className="mb-5">x</p>
       <p className="mb-5">x</p>
       <p className="mb-5">x</p>
