b8358bb2110d4516e1a15d361196b4c6ac333dc9
Simone Poggiali <gibbok.coding@gmail.com>
2025-01-01 11:41:53 +0100

    s

---
+++ my-app/app/server-actions/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div>
+      <h2 className="text-3xl mb-5">Server Actions and Mutations</h2>
+      <p className="mb-5">
+        Server Actions are asynchronous functions that are executed on the
+        server. They can be called in Server and Client Components to handle
+        form submissions and data mutations in Next.js applications.
+      </p>
+      <p className="mb-5">x</p>
+      <p className="mb-5">x</p>
+      <p className="mb-5">x</p>
+      <p className="mb-5">x</p>
+    </div>
+  )
+}
